647b52f6f204b3032f4105c1a326050208dd9f03
phunlh2001 <phunlh2001@gmail.com>
2025-01-03 09:35:07 +0700

    fix: non-nullable

--- DTOs/AddTaskRequest.cs
+++ DTOs/AddTaskRequest.cs
@@ -2,7 +2,7 @@
 {
     public class AddTaskRequest
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
     }
 }
